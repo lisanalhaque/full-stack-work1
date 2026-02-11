4350d7532005c2a9b1ff81e09110d2f1f18cea31
lisanalhaque <lisanop4444@gmail.com>
2026-02-11 11:06:13 +0530

    11-02-2026

--- Server/controllers/auth.controllers.js
+++ Server/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")                                             
 
 const createToken = async (req, res) => {
     try {
-        const token = jwt.sign({ sub: "testing" }, "jgrskygchljc", { expiresIn: "1h" })                    // Create a JWT with a payload, secret key, and expiration time
+        const token = jwt.sign({ sub: "testing" }, "jgrskygchljc", { expiresIn: "10" })                    // Create a JWT with a payload, secret key, and expiration time
         res.status(200).json({ message: "Token created successfully", token: token })
     } catch (err) {
         return res.status(500).json({ message: "Error creating token", error: err })
